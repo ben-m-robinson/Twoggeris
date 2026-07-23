0c08666bc875389110dda6695b351580fe993e4e
ben-m-robinson <ben.moir.robinson@gmail.com>
2026-07-23 11:30:54 +0100

    added 6 letter wordle game mode

--- Cogs/wordle_commands.py
+++ Cogs/wordle_commands.py
@@ -3,6 +3,7 @@ from wordle.wordle import WordleGame
 
 games = {}
 
+
 def format_game_state(result):
     symbols = {
         "grey": "⬛ ",
@@ -28,15 +29,23 @@ class WordleCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
     @commands.command()
     async def wordle(self, ctx):
-        games[ctx.author.id] = WordleGame()
-        await ctx.send("New Wordle game started! You have 6 guesses.")
+        games[ctx.author.id] = WordleGame(word_length=5)
+        await ctx.send("New 5-letter Wordle game started! You have 6 guesses.")
+
+
+    @commands.command()
+    async def wordle6(self, ctx):
+        games[ctx.author.id] = WordleGame(word_length=6)
+        await ctx.send("New 6-letter Wordle game started! You have 6 guesses.")
+
 
     @commands.command()
     async def guess(self, ctx, word):
         if ctx.author.id not in games:
-            await ctx.send("Start a game first with !wordle")
+            await ctx.send("Start a game first with !wordle or !wordle6")
             return
 
         game = games[ctx.author.id]
--- wordle/wordle.py
+++ wordle/wordle.py
@@ -1,6 +1,8 @@
 from .words import WORDS
+from .six_letter_words import SIX_LETTER_WORDS
 import random
 from enum import IntEnum
+
 
 class LetterState(IntEnum):
     UNKNOWN = 0
@@ -8,39 +10,55 @@ class LetterState(IntEnum):
     YELLOW = 2
     GREEN = 3
 
+
 class WordleGame:
-    def __init__(self):
-        self.word = random.choice(WORDS)
+    def __init__(self, word_length=5):
+        self.word_length = word_length
+
+        if word_length == 5:
+            self.word = random.choice(WORDS)
+        elif word_length == 6:
+            self.word = random.choice(SIX_LETTER_WORDS)
+        else:
+            raise ValueError("Unsupported word length")
+
         self.guesses_remaining = 6
         self.game_won = False
+
         self.letters = {
-        chr(i): LetterState.UNKNOWN
-        for i in range(ord("a"), ord("z") + 1)
-    }
-    
+            chr(i): LetterState.UNKNOWN
+            for i in range(ord("a"), ord("z") + 1)
+        }
+
+
     def make_guess(self, guess):
         guess = guess.lower()
-        if len(guess) != 5:
-            return "Guess must be 5 letters"
-        
+
+        if len(guess) != self.word_length:
+            return f"Guess must be {self.word_length} letters"
+
         self.guesses_remaining -= 1
 
         guess_list = list(guess)
         split_word = list(self.word)
-        score = [LetterState.GREY] * 5
 
-        for i in range(5):
+        score = [LetterState.GREY] * self.word_length
+
+
+        for i in range(self.word_length):
             if guess_list[i] == split_word[i]:
                 score[i] = LetterState.GREEN
                 guess_list[i] = None
                 split_word[i] = None
 
-        for i in range(5):
+
+        for i in range(self.word_length):
             if guess_list[i] is not None and guess_list[i] in split_word:
                 score[i] = LetterState.YELLOW
                 split_word[split_word.index(guess_list[i])] = None
-        
-        if score == [LetterState.GREEN] * 5:
+
+
+        if score == [LetterState.GREEN] * self.word_length:
             self.game_won = True
 
         for letter, state in zip(guess, score):
@@ -49,10 +67,10 @@ class WordleGame:
 
 
         return {
-        "score": [state.name.lower() for state in score],
-        "letters": {
-         letter: state.name.lower()
-            for letter, state in self.letters.items()
-        },
-        "guesses_remaining": self.guesses_remaining,
-}
+            "score": [state.name.lower() for state in score],
+            "letters": {
+                letter: state.name.lower()
+                for letter, state in self.letters.items()
+            },
+            "guesses_remaining": self.guesses_remaining,
+        }
