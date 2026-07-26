df47c471f1afb89d244f714196191188c3e0261f
ben-m-robinson <ben.moir.robinson@gmail.com>
2026-07-26 21:39:54 +0100

    fixed bug

--- Cogs/wordle_commands.py
+++ Cogs/wordle_commands.py
@@ -35,27 +35,20 @@ def format_quordle_state(results, guesses_remaining):
 
     message = ""
 
-    # Boards
     for i, result in enumerate(results):
         message += f"Word {i + 1}\n"
         message += "".join(symbols[x] for x in result["score"])
         message += "\n\n"
 
 
-    # Per word letters
     message += "Letters by word:\n\n"
 
     for i, result in enumerate(results):
         message += f"Word {i + 1}:\n"
-
-        for letter, state in result["letters"].items():
+    for letter, state in result["letters"].items():
+        if state != "unknown":
             message += f"{letter.upper()}: {symbols[state]}\n"
-
-        message += "\n"
-
-
     message += f"Guesses remaining: {guesses_remaining}"
-
     return message
 
 
